b88895bf3da2a296ee37d7f9742f31ad91a0fbb9
chikamim <chikami@exdit.com>
2016-11-24 10:58:07 +0900

    update Casks

--- Casks/finder-newfile.rb
+++ Casks/finder-newfile.rb
@@ -2,7 +2,7 @@ cask 'finder-newfile' do
   version '1.0.0'
   sha256 '50728d5ee84a9610ae24f31347d8aac7ca702c3eaa71910b2b52bf6646ea8aa8'
 
-  url 'https://github.com/chikamim/homebrew-tap/raw/master/binaries/finder-newfile.zip'
+  url 'https://github.com/chikamim/homebrew-tap/releases/download/2016-11-24/finder-newfile.zip'
   homepage ''
 
   app 'finder_newfile.app'
--- Casks/xee2.rb
+++ Casks/xee2.rb
@@ -1,8 +1,8 @@
-cask 'xee' do
+cask 'xee2' do
   version '2.2'
   sha256 'de625523ccf5091c2a42cc1e16eed3b5e80407742c45b9a7e09257d1c03194f2'
 
-  url 'https://github.com/chikamim/homebrew-tap/raw/master/binaries/xee2.zip'
+  url 'https://github.com/chikamim/homebrew-tap/releases/download/2016-11-24/xee2.zip'
   homepage ''
 
   app 'Xee.app'
